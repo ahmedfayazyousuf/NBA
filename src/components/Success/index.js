3bd4d4f16b2720b32ce217aa254611f400498e74
ahmedfayazyousuf <95180025+ahmedfayazyousuf@users.noreply.github.com>
2023-02-22 13:03:26 +0400

    i
    i

--- src/components/Success/index.js
+++ src/components/Success/index.js
@@ -3,12 +3,12 @@ import basketball from '../images/basketball.jpg'
 
 const Success = () => {
     return(
-        <div style={{display:"flex", flexDirection:"column", width:"100vw", height: "100vh", justifyContent:"center", alignItems:"center"}}>
+        <div style={{display:"flex", flexDirection:"column", width:"100vw", height: "100%", justifyContent:"center", alignItems:"center"}}>
             
             <div style={{display:"flex", flexDirection:"column", width:"100%", height: "100%", justifyContent:"center", alignItems:"center", flexWrap: 'wrap', textAlign: 'center'}}>
                 <img style={{minWidth: '100px', maxWidth: '300px'}} src={tick} alt="tick"/>
                 <h1 style={{paddingRight: '50px', paddingLeft: '50px', marginBottom: '-10px', color: 'white'}}>Your response has been recorded</h1>
-                <h1 style={{paddingRight: '50px', paddingLeft: '50px', color: 'red'}}>Thank You!</h1>
+                <h1 style={{paddingRight: '50px', paddingLeft: '50px', color: 'red', marginTop: '10px'}}>Thank You!</h1>
             </div>
         
         </div>
