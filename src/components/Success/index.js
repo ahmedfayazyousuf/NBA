f5fb4f77117deedccc468efc9c1f85144216ec86
ahmedfayazyousuf <95180025+ahmedfayazyousuf@users.noreply.github.com>
2023-02-22 11:15:26 +0400

    o
    o

--- src/components/Success/index.js
+++ src/components/Success/index.js
@@ -3,15 +3,15 @@ import basketball from '../images/basketball.jpg'
 
 const Success = () => {
     return(
-        <div style={{display:"flex", flexDirection:"column", width:"100vw", height: "100vh", justifyContent:"center", alignItems:"center"}}>
+        <div style={{display:"flex", flexDirection:"column", width:"100vw", height: "100%", justifyContent:"center", alignItems:"center"}}>
             
-            <div style={{display:"flex", flexDirection:"column", width:"100%", height: "75vh", justifyContent:"center", alignItems:"center", flexWrap: 'wrap', textAlign: 'center'}}>
+            <div style={{display:"flex", flexDirection:"column", width:"100%", height: "75%", justifyContent:"center", alignItems:"center", flexWrap: 'wrap', textAlign: 'center'}}>
                 <img style={{minWidth: '100px', maxWidth: '300px',}} src={tick} alt="tick"/>
                 <h1 style-={{paddinRight: '50px', paddingLeft: '50px', marginBottom: '100px'}}>Your response has been recorded</h1>
                 <h1 style-={{paddinRight: '50px', paddingLeft: '50px', marginTop: '-200px'}}>Thank You!</h1>
             </div>
 
-            <div style={{display:"flex", flexDirection:"column", width:"100vw", height: "25vh", justifyContent:"center", alignItems:"center"}}>
+            <div style={{display:"flex", flexDirection:"column", width:"100vw", height: "25%", justifyContent:"center", alignItems:"center"}}>
                 <div style={{display: 'flex', width: '100%', justifyContent: 'flex-end', alignItems: 'end'}}>
                     <img style={{width: '170px', marginRight: '30px', marginBottom: '20px'}} src={basketball} alt="basketball"/>
                 </div>
