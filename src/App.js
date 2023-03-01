19c7f2ddad325224ae51c2b0e9c7ad489eeb172e
ahmedfayazyousuf <95180025+ahmedfayazyousuf@users.noreply.github.com>
2023-03-01 14:46:24 +0400

    q
    q

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import "./App.css";
 
 import Success from './components/Success';
 import Registration from './components/Registration';
+import RaffleRegistration from './components/RaffleRegistration';
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
       <Routes>
           <Route exact path="/" element={<Registration />} />
           <Route path="/Success" element={<Success />} />
+          <Route path="/RaffleRegistration" element={<RaffleRegistration />} />
       </Routes>
     </>
   )
