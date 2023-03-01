31ef26049a1d2019b619575838ac24710bf813bb
ahmedfayazyousuf <95180025+ahmedfayazyousuf@users.noreply.github.com>
2023-03-01 15:45:39 +0400

    o
    o

--- src/components/RaffleRegistration/index.js
+++ src/components/RaffleRegistration/index.js
@@ -121,7 +121,7 @@ const Registration = () =>{
     
     return( 
             
-        <div style={{display:"flex", flexDirection:"column", width:"100vw", height: "100%", justifyContent:"center", alignItems:"center"}}>
+        <div style={{display:"flex", flexDirection:"column", width:"100vw", height: "100vh", justifyContent:"center", alignItems:"center"}}>
 
             <div style={{display: 'flex', flexDirection: 'column', width: '70%', gap:'5px', alignItems: 'center', justifyContent:'center'}}>
 
